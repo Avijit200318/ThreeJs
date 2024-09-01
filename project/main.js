cc4d99f165481e4770058074fb286a6f34a12c1f
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-09-01 13:58:15 +0530

    render the scene

--- project/main.js
+++ project/main.js
@@ -8,6 +8,14 @@ const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"});
 const cubeMesh = new THREE.Mesh(
   cubeGeometry, cubeMaterial
 )
-
 scene.add(cubeMesh);
-console.log(scene);
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 30);
+
+camera.position.z = 5;
+
+const canvas = document.querySelector("canvas.threejs");
+
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
