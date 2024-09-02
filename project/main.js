f9416fd16c6f5bf0b3658d97472c7c448d673803
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-09-02 20:11:01 +0530

    added orbit controlls

--- project/main.js
+++ project/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 
@@ -10,12 +11,25 @@ const cubeMesh = new THREE.Mesh(
 )
 scene.add(cubeMesh);
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 30);
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 200);
 
 camera.position.z = 5;
 
 const canvas = document.querySelector("canvas.threejs");
 
 const renderer = new THREE.WebGLRenderer({canvas});
+
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
+
+// inisilize the controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
+
+const renderLoop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(renderLoop);
+}
+
+renderLoop();
