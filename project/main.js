1eb77d6823ad8fd26240a1f52c88f8370a1f5687
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-09-02 20:54:01 +0530

    resize problem concept

--- project/main.js
+++ project/main.js
@@ -11,7 +11,15 @@ const cubeMesh = new THREE.Mesh(
 )
 scene.add(cubeMesh);
 
+// prespective camera
+
 const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 200);
+
+// orthograpic camera
+
+// const aspectRatio = window.innerWidth / window.innerHeight;
+// // aspect raito is needed for orthograpicCamera other wise it streach the object the we want to view in screen view size.
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 200);
 
 camera.position.z = 5;
 
@@ -26,7 +34,15 @@ const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.autoRotate = true;
 
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
 const renderLoop = () => {
+
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(renderLoop);
