8da8ac259e05de2a38b98a17466b904334df6de8
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-09-03 12:15:07 +0530

    group and some methods

--- project/main.js
+++ project/main.js
@@ -6,17 +6,31 @@ const scene = new THREE.Scene();
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"});
 
-const cubeMesh = new THREE.Mesh(
-  cubeGeometry, cubeMaterial
-)
-scene.add(cubeMesh);
-// mesh position
-// cubeMesh.position.y = 1;
-// cubeMesh.position.x = 1;
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+cubeMesh.position.y = -1;
+// this position is locally relative. since the group is already position to y axis at 2 so it set the position based on the group means 2-1 = 1.
+cubeMesh.scale.setScalar(0.5);
+// 0.5 * 2 = 1
+const cubeMesh2 = new THREE.Mesh(cubeGeometry, cubeMaterial);
+cubeMesh2.position.x = 2;
+const cubeMesh3 = new THREE.Mesh(cubeGeometry, cubeMaterial);
+cubeMesh3.position.x = -2;
 
-// vector
-const tempVector = new THREE.Vector3(0, 3, 0);
-cubeMesh.position.copy(tempVector);
+const group = new THREE.Group();
+group.add(cubeMesh);
+group.add(cubeMesh2);
+group.add(cubeMesh3);
+
+// group.scale.y = 2;
+// if we scale, position the parent then also the child scale.
+group.position.y = 2;
+group.scale.setScalar(2) 
+// this line increase the all side of the group size by 2
+
+scene.add(group);
+// scene.add(cubeMesh);
+
+// cubeMesh.scale.set(2,2 ,1)
 
 const axisHelper = new THREE.AxesHelper(2);
 scene.add(axisHelper);
