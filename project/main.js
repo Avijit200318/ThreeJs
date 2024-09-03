b6f9b9eafcc66a71f8916e532f5d9b12ae3c66a7
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-09-03 11:35:10 +0530

    mesh position, vector3, camera and mesh position

--- project/main.js
+++ project/main.js
@@ -10,35 +10,39 @@ const cubeMesh = new THREE.Mesh(
   cubeGeometry, cubeMaterial
 )
 scene.add(cubeMesh);
+// mesh position
+// cubeMesh.position.y = 1;
+// cubeMesh.position.x = 1;
+
+// vector
+const tempVector = new THREE.Vector3(0, 3, 0);
+cubeMesh.position.copy(tempVector);
+
+const axisHelper = new THREE.AxesHelper(2);
+scene.add(axisHelper);
 
 // prespective camera
 
 const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 200);
 
-// orthograpic camera
-
-// const aspectRatio = window.innerWidth / window.innerHeight;
-// // aspect raito is needed for orthograpicCamera other wise it streach the object the we want to view in screen view size.
-// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 200);
 
 camera.position.z = 5;
+
+// distance between the mesh and the camera
+console.log(cubeMesh.position.distanceTo(camera.position))
 
 const canvas = document.querySelector("canvas.threejs");
 
 const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
-// use antialias: true which help us to fix show pixel view smoothly. This is a software solution.
 
-// console.log(window.devicePixelRatio) 1.25 for my laptop
+
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-// because mobile device contain 4k or hight pixel. we dont need that much.
-// this is a hardware solution which help us to show pixel smoothly.
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 // inisilize the controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-controls.autoRotate = true;
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
