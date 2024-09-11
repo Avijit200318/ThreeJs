da744d08072f14df7311e8ad82e5c9f8b76c7173
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-09-11 11:54:02 +0530

    add texture to the gemotry

--- project/script.js
+++ project/script.js
@@ -8,61 +8,60 @@ const pane = new Pane();
 // initialize the scene
 const scene = new THREE.Scene();
 
+// initialize the loader
+const textureLoader = new THREE.TextureLoader();
+
 // initialize the geometry
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const planeGeometry = new THREE.PlaneGeometry(1, 1);
 const touristKnotGeometry = new THREE.TorusKnotGeometry(0.5, 0.15, 100, 16);
+const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 32);
+
+// initialize the texture
+const textureTest = textureLoader.load('/texture/whispy-grass-meadow-bl/wispy-grass-meadow_albedo.png');
+// console.log(textureTest)
+
 
 // initialize the material
-const material = new THREE.MeshPhysicalMaterial();
-material.color = new THREE.Color('green');
+const material = new THREE.MeshBasicMaterial();
+material.map = textureTest;
+material.color = new THREE.Color("red");
 
-pane.addBinding(material, 'metalness', {
-  min: 0,
-  max: 1,
-  step: 0.01
-})
-pane.addBinding(material, 'roughness', {
-  min: 0,
-  max: 1,
-  step: 0.01
-})
-pane.addBinding(material, 'reflectivity', {
-  min: 0,
-  max: 1,
-  step: 0.01
-})
-pane.addBinding(material, 'clearcoat', {
-  min: 0,
-  max: 1,
-  step: 0.01
-})
+// create group
+const group = new THREE.Group();
 
 // initialize the mesh
 const mesh = new THREE.Mesh(geometry, material);
 const mesh2 = new THREE.Mesh(touristKnotGeometry, material);
 const plane = new THREE.Mesh(planeGeometry, material);
-// mesh.castShadow = true;
 
 mesh2.position.x = 1.5;
 plane.position.x = -1.5;
 
-scene.add(mesh);
-scene.add(mesh2);
+// we can also define it this way
+const sphere = new THREE.Mesh();
+sphere.geometry = sphereGeometry;
+sphere.material = material;
+
+const cylinder = new THREE.Mesh(cylinderGeometry, material);
+
+sphere.position.y = 1.5;
+cylinder.position.y = -1.5;
+
 scene.add(plane);
+scene.add(cylinder);
+scene.add(sphere);
+// we can use this also
+// scene.add(sphere, cylinder);
+group.add(mesh2, mesh);
+scene.add(group);
 
 // set light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2.5);
-directionalLight.position.set(5, 5, 5);
-directionalLight.castShadow = true;  // Enable shadows for the directional light
-directionalLight.shadow.mapSize.width = 1024;  // Adjust shadow quality
-directionalLight.shadow.mapSize.height = 1024;
-directionalLight.shadow.camera.near = 0.5;
-directionalLight.shadow.camera.far = 20;
+const light = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(light);
 
-scene.add(directionalLight);
-
-const pointLight = new THREE.PointLight(0xffffff, 1.5);
+const pointLight = new THREE.PointLight(0xffffff, 1.2);
 pointLight.position.set(5, 5, 5);
 scene.add(pointLight);
 
@@ -98,6 +97,20 @@ window.addEventListener("resize", () => {
 
 // render the scene
 const renderloop = () => {
+
+  // scene.children.forEach((child) => {
+  //   if(child instanceof THREE.Mesh){
+  //     child.rotation.y += 0.01;
+  //   }
+  // })
+  // but the proble of this method is if we have to much mesh and we want only some of them to rotate then we have to create a group.
+
+  group.children.forEach((child) => {
+    if(child instanceof THREE.Mesh){
+      child.rotation.y += 0.01;
+    }
+  })
+
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(renderloop);
