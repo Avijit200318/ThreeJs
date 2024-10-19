2244d325601bba0823cc0c6eaa4c0102a3ab748d
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-10-19 20:08:37 +0530

    added 3d models with hdri light inside three js page

--- landingPage/main.js
+++ landingPage/main.js
@@ -1,30 +1,55 @@
 import * as THREE from "three";
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+// import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+// import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 
 const scene = new THREE.Scene();
 
 // geometry
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 
 // material
-const material = new THREE.MeshBasicMaterial({color: "red"});
 
 // meshes
-const sphereMesh = new THREE.Mesh(cubeGeometry, material);
 
-scene.add(sphereMesh);
 
 // camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 100);
 
-camera.position.z = 4;
+camera.position.z =4;
 
 const canvas = document.querySelector("canvas.threejs");
 
-const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 
 renderer.setPixelRatio(Math.min(2, window.devicePixelRatio));
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+// update rendere to show the hdri colors
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+renderer.outputEncoding = THREE.sRGBEncoding;
+// renderer.outputColorSpace = 'srgb';
+
+const pmremGenerator = new THREE.PMREMGenerator(renderer);
+pmremGenerator.compileEquirectangularShader();
+
+new RGBELoader().load('/pond_bridge_night_1k.hdr', (texture) => {
+  const envMap = pmremGenerator.fromEquirectangular(texture).texture;
+  scene.background = envMap;
+  scene.environment = envMap;
+  texture.dispose();
+  pmremGenerator.dispose();
+
+  const loader = new GLTFLoader();
+
+  loader.load('./DamagedHelmet.gltf', (gltf) => {
+    scene.add(gltf.scene);
+  }, undefined, (error) => {
+    console.log("An error occured while loading the GLTF model: ", error);
+  });
+})
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
@@ -37,7 +62,7 @@ window.addEventListener('resize', () => {
 })
 
 const rendererLoop = () => {
-  
+
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(rendererLoop);
